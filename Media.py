31be0037f82ea4a4bb760e78aeeed52de467fb4c
edgarponcee <poncegozalvezedgar@gmail.com>
2025-09-19 09:01:10 +0200

    Add files via upload

---
+++ Media.py
@@ -0,0 +1,12 @@
+print('Dame el primer numero entero:')
+n1 = int(input())
+print('Dame el segundo numero entero:')
+n2 = int(input())
+print('Dame el tercero numero entero:')
+n3 = int(input())
+print('Dame el cuarto numero entero:')
+n4 = int(input())
+
+media = (n1 + n2 + n3 + n4) / 4
+
+print("La media es %.3f" % media)
